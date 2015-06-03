0d509676a1ccdb9acc292085e8ea63bb7d1ab041
Kabum42 <al264084@uji.es>
2015-06-03 19:54:35 +0200

    New and better layouts

--- app/src/main/java/com/example/ams/brewed/activities/ResultsActivity.java
+++ app/src/main/java/com/example/ams/brewed/activities/ResultsActivity.java
@@ -15,7 +15,7 @@ public class ResultsActivity extends ActionBarActivity implements IResultsView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_results);
         initializeActivity();
         viewmodel = Viewmodel.getInstance();
     }
