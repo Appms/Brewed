bd7ebc88b4bac8fd9bcab90c1f20d8e512c118ed
Appms <amserik@hotmail.com>
2015-06-04 11:29:39 +0200

    Search Dialogs implemented

--- app/src/main/java/com/example/ams/brewed/activities/ResultsActivity.java
+++ app/src/main/java/com/example/ams/brewed/activities/ResultsActivity.java
@@ -2,7 +2,12 @@ package com.example.ams.brewed.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.ams.brewed.MessageFragment;
 import com.example.ams.brewed.R;
 import com.example.ams.brewed.Viewmodel;
 import com.example.ams.brewed.data.Brewery;
@@ -17,21 +22,46 @@ public class ResultsActivity extends ActionBarActivity implements IResultsView {
     Viewmodel viewmodel;
     private Viewmodel.SearchType searchType;
 
+    private TextView searchCriteria;
+    private ListView listView;
+    private ProgressBar searching;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-        //initializeActivity();
+
+        initializeActivity();
+
         viewmodel = Viewmodel.getInstance();
         viewmodel.storeResultsActivity(this);
+
         searchType = viewmodel.currentSearchType;
         changeSearchType(searchType);
+    }
+
+    private void initializeActivity() {
+        searchCriteria = (TextView) findViewById(R.id.searchCriteria);
+        listView = (ListView) findViewById(R.id.listView);
+        searching = (ProgressBar) findViewById(R.id.searching);
     }
 
     protected void onResume()
     {
         super.onResume();
-        //viewmodel.connectView(this);
+        searchType = viewmodel.currentSearchType;
+        changeSearchType(searchType);
+    }
+
+    //ASKING FOR A BEER // BREWERY
+
+    public void onSearchCriteriaPressed(View view){
+        viewmodel.onSearchRequested();
+    }
+
+    @Override
+    public void askForSearchCriteria() {
+        new MessageFragment().show(getFragmentManager(), "Enter Search Criteria: ");
     }
 
     @Override
@@ -61,11 +91,6 @@ public class ResultsActivity extends ActionBarActivity implements IResultsView {
 
     @Override
     public void showBreweryResults(Brewery[] response) {
-
-    }
-
-    @Override
-    public void askForSearchCriteria() {
 
     }
 
